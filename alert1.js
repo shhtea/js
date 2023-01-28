45c3ed513c5554a772b2e47668a6f1192290de63
shhtea <kupol.85@mail.ru>
2023-01-28 23:32:39 +0400

    new

--- alert1.js
+++ alert1.js
@@ -1,11 +1,7 @@
-let num;
 
-do {
-  num = prompt("Введите число больше 100?", 0);
-} while (num <= 100 && num);
 
-let entNum
-do {
-  entNum = prompt('Введите число больше 100', '');
-}  while (entNum <= 100 && entNum);
+let newStr;
+newStr == 2;
+alert(typeof(newStr));
+
 
