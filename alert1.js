45ca62f7ef925e8a635b883b72c6b77be11f1af0
shhtea <kupol.85@mail.ru>
2023-02-01 00:01:59 +0400

    new

--- alert1.js
+++ alert1.js
@@ -4,4 +4,18 @@ let newStr;
 newStr == 2;
 alert(typeof(newStr));
 
+function letterCase(a,b) {
+    if (typeof a !== 'string' || typeof b !== 'string') {
+      alert(-1);
+    } else {
+      if (('a'.toUpperCase() == a) && ('b'.toUpperCase() == b)
+      || ('a'.toLowerCase() == a) && ('b'.toLowerCase() == b)) {
+        alert(1);
+      } else {
+        alert(0);
+      }
+    }
+  }
+  letterCase();
+
 
