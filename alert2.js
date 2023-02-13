f11b294637132a39ff23d7d79947071734d2b215
shhtea <kupol.85@mail.ru>
2023-02-13 23:52:11 +0400

    new

---
+++ alert2.js
@@ -0,0 +1,7 @@
+let fls = 'фрилансер';
+let text = `Привет! Я ${fls}`;
+console.log(text);
+console.log(text[2]);
+console.log(text.toUpperCase());
+console.log(fls.slice(3,6));
+console.log(fls.includes('лан',4));
